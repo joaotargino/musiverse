716b1cfff21061cd4f9cd07c91023d89cf271920
Ricardo Araújo <phd.ricardo@gmail.com>
2015-05-31 18:09:42 -0300

    no echo

--- scripts/similarity.sh
+++ scripts/similarity.sh
@@ -5,8 +5,6 @@ size=$2
 
 filename=`find . -iname "${id}.csv"`
 
-echo $filename
-
 if [ -f ${filename} ]; then
   ranking=`Rscript similarity.R ${filename} ${size}`
   cat `echo $ranking | sed 's/.h5/.metadata.csv/g'`
