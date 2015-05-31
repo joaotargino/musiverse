8ada8c0efcc2819012f986b120706999621cb98b
Victor Andrade de Almeida <victor.almeida@ccc.ufcg.edu.br>
2015-05-31 18:25:31 -0300

    Flask

--- api.py
+++ api.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 def similarity():
     result = os.popen('bash similarity.sh ' + request.args.get('track_id') + '5')
     result = result.read().split('\n')
-
+    
     response = []
-
-    for song in result:
-        song = result.split(',')
-        song = {title: song[9], album: song[14], artist: song[18]}
+    
+    for song in result[:-1]:
+        song = song.split(',')
+        song = {'artist': song[9], 'album': song[14], 'title': song[18]}
         response.append(song)
-
+    
     response = make_response(response)
     response.headers['Access-Control-Allow-Origin'] = '*'
-
+    
     return response
 
 if __name__ == '__main__':
