20a6718de2ede5e649cd828d06fd9fc701ab4db5
Ricardo Araujo Santos <phd.ricardo@gmail.com>
2015-08-13 18:54:42 -0300

    new form with artist and song name

--- musiverse.js
+++ musiverse.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static('musiverse'));
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Listening on port 3000");
 });
--- musiverse/app.js
+++ musiverse/app.js
@@ -1,32 +1,66 @@
+angular.module('app', [])
+    .controller('controller', function ($scope, $http) {
 
-var app = angular.module('app', []);
+        $http.get('data/file.json').success(
+            function (data) {
+                data.sort(function (a, b) {
+                    if (a.artist_name === b.artist_name) {
+                        return a.title < b.title ? -1 : 1;
+                    } else {
 
-app.controller('controller', function($scope, $http) {
+                        return a.artist_name < b.artist_name ? -1 : 1;
+                    }
+                });
+                $scope.songs = data;
+            });
 
-    $http.get('data/file.json').success(function(data) {
-                                        data.sort(function(a,b){
-                                                  return a.title < b.title ? -1 : 1;
-                                                  })
-        $scope.songs = data;
-    });
+        $scope.search = function () {
 
-    $scope.search = function() {
+            var result = $(this).data('track');
+            //        for (var i = 0; i < $scope.songs.length; i++)
+            //            if ($scope.query == $scope.songs[i].title)
+            //                result = $scope.songs[i].track_id;
 
-        var result;
-        for(var i=0; i<$scope.songs.length; i++)
-            if($scope.query == $scope.songs[i].title)
-                result = $scope.songs[i].track_id;
+            console.log(result);
+            console.log($scope.query);
 
-        $http({
-             url: "http://localhost:5405/similarity",
-             method: "GET",
-             params: {
-                 track_id: result
-             }
-         })
+            $http({
+                url: "http://localhost:5405/similarity",
+                method: "GET",
+                params: {
+                    track_id: result
+                }
+            })
 
-            .success(function(data) {
+            .success(function (data) {
                 $scope.recomendation = data;
             })
+        };
+
+        $scope.searchArtist = function (term) {
+            Geolocator.searchFlight(term).then(function (countries) {
+                $scope.countries = countries;
+            });
+        }
+    })
+
+.directive('keyboardPoster', function ($parse, $timeout) {
+    var DELAY_TIME_BEFORE_POSTING = 0;
+    return function (scope, elem, attrs) {
+
+        var element = angular.element(elem)[0];
+        var currentTimeout = null;
+
+        element.oninput = function () {
+            var model = $parse(attrs.postFunction);
+            var poster = model(scope);
+
+            if (currentTimeout) {
+                $timeout.cancel(currentTimeout)
+            }
+            currentTimeout = $timeout(function () {
+                poster(angular.element(element).val());
+            }, DELAY_TIME_BEFORE_POSTING)
+        }
     }
 });
