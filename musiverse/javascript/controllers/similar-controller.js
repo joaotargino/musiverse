8387a4d2db0d6567ac221c44bbdd5462c536b788
Ricardo Araújo <phd.ricardo@gmail.com>
2015-08-14 16:50:05 -0300

    fixing results page

--- musiverse/javascript/controllers/similar-controller.js
+++ musiverse/javascript/controllers/similar-controller.js
@@ -1,12 +1,8 @@
 angular.module('app')
-    .controller('SimilarController', function ($scope, $http, Musiverse) {
-        //        $scope.searchSimilarSongs = function () {
-        var result = $scope.allsongs.filter(function (song, index) {
-            return song.title === $scope.song
-        })[0];
-        Musiverse.searchSimilarSongs(result.track_id).then(function (songs) {
+    .controller('SimilarController', function ($scope, $routeParams, Musiverse) {
+        var controller = this;
+        Musiverse.searchSimilarSongs($routeParams.track_id).then(function (songs) {
+            $scope.song = songs.shift();
             $scope.songs = songs;
-            console.log(songs);
         });
-        //        };
     });
